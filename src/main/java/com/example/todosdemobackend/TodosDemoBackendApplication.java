205031a248aeacbb95890214ec7c1cdb9850bc29
Thomas Gotwig <tgotwig@gmail.com>
2020-02-29 11:04:07 +0100

    Initial commit by start.spring.io

---
+++ src/main/java/com/example/todosdemobackend/TodosDemoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.todosdemobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodosDemoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodosDemoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/todosdemobackend/TodosDemoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todosdemobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosDemoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
